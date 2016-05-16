b8e97a0369169f2b3a5bc4e13c31038dbee144ee
Mauricio Serna <mauricioserf@hotmail.com>
2016-05-15 23:29:32 -0500

    Add validation of length on movie desipcrition, add tawk for online chat

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,13 @@
 class Movie < ActiveRecord::Base
   extend WillPaginate
+
   acts_as_votable
+
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   validates :title, :description, presence: true
+  validates :description, length: { maximum: 250 }
 
   belongs_to :user
 end
